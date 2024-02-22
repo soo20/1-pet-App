7a8e12696a9601d9d8570e7c37a0c8befd5ea0f4
malk haitham <7pointersteam@gmail.com>
2024-02-22 12:57:44 +0200

    add some comments to explain the code update

--- lib/pages/splashscreen.dart
+++ lib/pages/splashscreen.dart
@@ -45,36 +45,54 @@ class _SplashScreenState extends State<SplashScreen>
       backgroundColor: Color(backgroundColor),
       body: Center(
         child: Column(
+          /*In order not to make the column take up all the available
+           space on the screen, you must make it take the minimum space,
+            which is the appropriate space for the column’s content.*/
           mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
               'assets/image/Group286.png',
+              //set the width of image
               width: 694.w,
-              //height: 808.48.h,
-              // Adjust the fit as needed
             ),
+            //you have two different text styles, so we make two text widgets.
             Padding(
+              /*We put it in the padding widget because we don't want any
+               height but want space between the image and the text.*/
               padding: const EdgeInsets.only(top: 10.0),
               child: Text(
-                'Hello,', // Replace with your desired text
+                // we put the text string.
+                'Hello,',
+                //to style your text:
                 style: TextStyle(
+                  //to make adaptive font size we call 'h' function:
                   height: 2.h,
-                  fontFamily: 'Cosffira', // Use Poppins font
-                  fontSize: 147.sp, // Adjust the font size as needed
+                  //to customize your font.
+                  fontFamily: 'Cosffira',
+                  //to make adaptive font size we call 'sp' function.
+                  fontSize: 147.sp,
                   color: const Color.fromRGBO(11, 47, 55, 1),
+                  //to make your font bold or normal "regular".
                   fontWeight: FontWeight.bold,
-                  // Use FontWeight.bold for bold style
                 ),
               ),
             ),
+
             Text(
-              'I\'m Yuna. Ready to explore the world of pets together?', // Replace with your desired text
+              // we put the text string.
+              'I\'m Yuna. Ready to explore the world of pets together?',
+              //to style your text:
               style: TextStyle(
-                fontFamily: 'Cosffira', // Use Cosffira font
-                fontSize: 40.sp, // Adjust the font size as needed
+                fontFamily: 'Cosffira',
+                //to make adaptive font size we call 'sp' function.
+                fontSize: 40.sp,
                 color: const Color.fromRGBO(0, 0, 0, 1),
+                //to make your font normal "regular" or bold.
                 fontWeight: FontWeight.normal,
               ),
+              /*The content of the text was large, 
+                so it was moved to the front when displaying,
+                and to make it in the center, I put this argument*/
               textAlign: TextAlign.center,
             ),
           ],
