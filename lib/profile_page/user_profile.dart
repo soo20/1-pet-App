8d976b621761e77a2afcabaf0739f23f54e12177
soo597 <129106633+soo20@users.noreply.github.com>
2024-03-02 22:50:29 +0200

    soha

--- lib/profile_page/user_profile.dart
+++ lib/profile_page/user_profile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 
@@ -9,7 +9,7 @@ class ClipTry extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffEEEFEF),
+      backgroundColor: const Color(0xffEEEFEF),
       body: Stack(
         children: [
          Positioned(
