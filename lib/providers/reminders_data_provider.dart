df3dbdb56f9007bba8d462598f174ac22d8d1c33
malk haitham <7pointersteam@gmail.com>
2024-03-23 09:14:41 +0200

    finish scrolled error in home page

--- lib/providers/reminders_data_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:petapplication/pages/homepage/scrolled_reminder.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final remindersProvider = Provider((ref) {
-  return Reminders.reminderInformationList;
-});
