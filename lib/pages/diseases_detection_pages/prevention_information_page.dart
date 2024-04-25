fa66ebc53da1c16b696ab8666c0f83942c30c047
malk haitham <7pointersteam@gmail.com>
2024-04-25 14:56:33 +0200

    try to restore eny thing from soha workgit add .!

--- lib/pages/diseases_detection_pages/prevention_information_page.dart
+++ lib/pages/diseases_detection_pages/prevention_information_page.dart
@@ -1,8 +1,5 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:petapplication/pages/homepage/home_page_with_navigation.dart';
 
 class PreventionInformationPage extends StatefulWidget {
   const PreventionInformationPage({
